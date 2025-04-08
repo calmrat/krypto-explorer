4de364102b19ed9a10fc41a975e8292805338e85
Chris Ward <chris@calmrat.com>
2025-04-08 14:53:25 +0200

    + raw (uncahced) querying for a single token via coingecko directly working

--- src/qrypt/tokens/api.py
+++ src/qrypt/tokens/api.py
@@ -11,18 +11,19 @@ It also includes the necessary authentication and authorization mechanisms.
 
 """
 
-from datetime import datetime
-
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from qrypt.tokens.schema import TokenOut
 from qrypt.tokens.services.coingecko.adapter import CoinGeckoAdapter
 
 router = APIRouter(prefix="/api/v1/tokens", tags=["tokens"])
 
+type EndPointResponseTokenList = list[TokenOut]
+ResponseModel = TokenOut
 
-@router.get("/", response_model=list[TokenOut])
-async def list_tokens() -> list[TokenOut]:
+
+@router.get("/", response_model=EndPointResponseTokenList)
+async def list_tokens() -> EndPointResponseTokenList:
     """
     List all tokens.
 
@@ -31,6 +32,10 @@ async def list_tokens() -> list[TokenOut]:
 
     client = CoinGeckoAdapter()
     coins = await client.api.coins_list()
+    if not coins:
+        return []
+
+    last_updated = None
 
     return [
         TokenOut(
@@ -38,7 +43,40 @@ async def list_tokens() -> list[TokenOut]:
             symbol=coin.get("symbol", ""),
             name=coin.get("name", ""),
             platforms=coin.get("platforms", {}),
-            last_updated=datetime.now(),
+            last_updated=last_updated,
         )
         for coin in coins
     ]
+
+
+@router.get("/{token_id}", response_model=TokenOut)
+async def get_token(token_id: str) -> TokenOut:
+    """
+    Get a token by its ID.
+
+    Args:
+        token_id (str): The ID of the token to retrieve.
+
+    Returns:
+        TokenOut: The token with the specified ID.
+    """
+
+    client = CoinGeckoAdapter()
+    coins = await client.api.coins_list()
+    if not coins:
+        raise HTTPException(status_code=404, detail=f"Token not found [{token_id}]")
+
+    # Find the token with the specified ID
+    coin = next((c for c in coins if c["id"] == token_id), None)
+    if not coin:
+        raise HTTPException(status_code=404, detail=f"Token not found [{token_id}]")
+
+    last_updated = None
+
+    return TokenOut(
+        id=coin.get("id", ""),
+        symbol=coin.get("symbol", ""),
+        name=coin.get("name", ""),
+        platforms=coin.get("platforms", {}),
+        last_updated=last_updated,
+    )
