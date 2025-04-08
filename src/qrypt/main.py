989137540f717eee10968aa9574ac1a0314693b7
Chris Ward <chris@calmrat.com>
2025-04-08 14:01:13 +0200

    initial app + router setup

--- src/qrypt/main.py
+++ src/qrypt/main.py
@@ -5,3 +5,11 @@ Qrypto - Main Entry Point
 
 This module serves as the main entry point for the Qrypto application.
 """
+
+from fastapi import FastAPI
+
+from qrypt.tokens.api import router
+
+app = FastAPI(title="Crypto Records Manager")
+
+app.include_router(router)
